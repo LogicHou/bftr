26b3e0e0a9214db470f7fdd6b7d083640c598976
LogicHou <logichou@qq.com>
2022-02-28 20:55:51 +0800

    更新了一些逻辑骨架

--- internal/store/memstore.go
+++ internal/store/memstore.go
@@ -3,7 +3,8 @@ package store
 import (
 	"sync"
 
-	"github.com/LogicHou/bftr/store/factory"
+	tstore "github.com/LogicHou/bftr/store"
+	factory "github.com/LogicHou/bftr/store/factory"
 )
 
 func init() {
@@ -12,4 +13,23 @@ func init() {
 
 type MemStore struct {
 	sync.RWMutex
+	trader *tstore.Trader
+}
+
+// 市价下单函数 RestAPI
+func (ms *MemStore) CreateMarketOrder(trader *tstore.Trader) error {
+
+	return nil
+}
+
+// 平仓现有的所有仓位函数
+func (ms *MemStore) ClosePosition(trader *tstore.Trader) error {
+
+	return nil
+}
+
+// 可能用来刷新一些数据
+func (ms *MemStore) RefreshData(trader *tstore.Trader) error {
+
+	return nil
 }
--- store/store.go
+++ store/store.go
@@ -1,13 +1,45 @@
 package store
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/adshao/go-binance/v2/futures"
+)
 
 var (
 	ErrNotFound = errors.New("not found")
 	ErrExist    = errors.New("exist")
 )
 
-type Wsk struct {
+type Trader struct {
+	pid
+	wsk
+}
+
+// 一些负责控制下单平仓的控制参数
+type pid struct {
+	margin      float64
+	marginRatio float64
+	maxVARS5    float64
+	maxVARS4    float64
+
+	posAmt         float64
+	entryPrice     float64
+	leverage       float64
+	posSide        futures.SideType
+	stopLoss       float64
+	firstStopLoss  float64
+	gainQty        int
+	interval       string
+	openVolume     float64
+	initOV         float64
+	closeMA        int
+	initCMA        int
+	boxMarginLimit int
+}
+
+// websocket中ticket的详细数据
+type wsk struct {
 	h  float64
 	l  float64
 	c  float64
